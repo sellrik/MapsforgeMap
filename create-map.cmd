73b6e3aeadb6b8d1f0b2a5aa12d67c3ad8d931ae
sellrik <you@example.com>
2022-04-16 09:26:52 +0200

    0416

--- create-map.cmd
+++ create-map.cmd
@@ -1,5 +1,6 @@
 echo ########## Converting pbf to osm ##########
 call lib\osmosis\bin\osmosis.bat --read-pbf data\hungary-latest.osm.pbf --write-xml data\map.osm
+REM call lib\osmosis\bin\osmosis.bat --read-pbf data\hungary-latest.osm.pbf --bounding-box top=47.655755 left=18.865715 bottom=47.480221 right=19.0719 --write-xml data\map.osm
 
 echo ########## Creating trailmarks ##########
 lib\transform\Transform.exe --source data\map.osm --target data\map_trailmarks.osm  
@@ -9,4 +10,4 @@ call lib\osmosis\bin\osmosis.bat --rx data/map.osm --rx data/out_file_lon16.11_2
 call lib\osmosis\bin\osmosis.bat --rx data/map_srtm.osm --rx data/map_trailmarks.osm --merge --wx data\map_srtm_trailmarks.osm
 
 echo ########## Creating map ##########
-call lib\osmosis\bin\osmosis.bat --read-xml data\map_srtm_trailmarks.osm --mw file=data\hungary.map tag-conf-file=tag-mapping_modified.xml
+call lib\osmosis\bin\osmosis.bat --read-xml data\map_srtm_trailmarks.osm --mw file=data\hungary.map tag-conf-file=tag-mapping.xml
